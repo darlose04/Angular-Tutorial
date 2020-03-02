9cf32a9d29bc5d7375a1fcf427b053b264d5fc30
Zach Smith <zsmith126028@gmail.com>
2020-03-02 10:50:43 -0600

    add route for shipping component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartComponent } from "./cart/cart.component";
-import { ShippingComponent } from './shipping/shipping.component';
+import { ShippingComponent } from "./shipping/shipping.component";
 
 @NgModule({
   imports: [
@@ -20,7 +20,8 @@ import { ShippingComponent } from './shipping/shipping.component';
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
-      { path: "cart", component: CartComponent }
+      { path: "cart", component: CartComponent },
+      { path: "shipping", component: ShippingComponent }
     ])
   ],
   declarations: [
