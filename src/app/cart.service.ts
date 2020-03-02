0a56c37179f3c6301b193d5df60070617374b307
Zach Smith <zsmith126028@gmail.com>
2020-03-02 10:40:13 -0600

    import HttpClient in cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 
 @Injectable({
   providedIn: "root"
@@ -19,5 +20,5 @@ export class CartService {
     return this.items;
   }
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 }
